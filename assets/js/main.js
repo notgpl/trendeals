be60f61ea7cf16dd86482380b9820ef056194842
Adarsh | ly <4darsh.as@gmail.com>
2025-12-23 10:39:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,21 +110,6 @@
     }
   });
 
-
-  /**
-   * Initiate Pure Counter
-   */
-  new PureCounter();
-
-  /**
-   * Frequently Asked Questions Toggle
-   */
-  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
-    faqItem.addEventListener('click', () => {
-      faqItem.parentNode.classList.toggle('faq-active');
-    });
-  });
-
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
