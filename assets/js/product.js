7208deb9e7a334d0a1a2287cbce9e070842c9347
Adarsh | ly <4darsh.as@gmail.com>
2025-12-25 13:07:33 +0530

    Update product.js

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,8 +1,4 @@
-const SUPABASE_URL = "https://ctiglwnyimjzocvuizdq.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWdsd255aW1qem9jdnVpemRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDI4MTMsImV4cCI6MjA4MTYxODgxM30.eW5nl5yCXnz0V0iya6cZCMj4Pgy05vhClUQ4pLfwYNI";
-
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const client = window.supabaseClient;
 
 let productName = "";
 let productPrice = 0;
