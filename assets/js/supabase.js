9279795e74ac662e7f482a03fbff7d5a009f3c2b
Adarsh | ly <4darsh.as@gmail.com>
2025-12-25 13:00:01 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,8 @@
 window.SUPABASE_URL = "https://ctiglwnyimjzocvuizdq.supabase.co";
-window.SUPABASE_KEY = "YOUR_KEY";
+window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWdsd255aW1qem9jdnVpemRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDI4MTMsImV4cCI6MjA4MTYxODgxM30.eW5nl5yCXnz0V0iya6cZCMj4Pgy05vhClUQ4pLfwYNI";
 
 window.supabaseClient = supabase.createClient(
   window.SUPABASE_URL,
   window.SUPABASE_KEY
 );
+
