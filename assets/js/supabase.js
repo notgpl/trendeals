427f40f315b57bab2aaa7a033239821eb43f743b
Adarsh | ly <4darsh.as@gmail.com>
2025-12-26 12:45:46 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,8 +1,7 @@
-window.SUPABASE_URL = "https://ctiglwnyimjzocvuizdq.supabase.co";
-window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWdsd255aW1qem9jdnVpemRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDI4MTMsImV4cCI6MjA4MTYxODgxM30.eW5nl5yCXnz0V0iya6cZCMj4Pgy05vhClUQ4pLfwYNI";
+const SUPABASE_URL = "https://ctiglwnyimjzocvuizdq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0aWdsd255aW1qem9jdnVpemRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDI4MTMsImV4cCI6MjA4MTYxODgxM30.eW5nl5yCXnz0V0iya6cZCMj4Pgy05vhClUQ4pLfwYNI";
 
 window.supabaseClient = supabase.createClient(
-  window.SUPABASE_URL,
-  window.SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
-
